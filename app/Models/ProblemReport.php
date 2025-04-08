2d0c61923099e9634b7f4bdf848f4731fe3195e3
Umer Farooq <umer99976@gmail.com>
2025-04-08 10:28:32 +0500

    Fetch company_id through job_uuid

--- app/Models/ProblemReport.php
+++ app/Models/ProblemReport.php
@@ -7,16 +7,16 @@ use Illuminate\Database\Eloquent\Model;
 class ProblemReport extends Model
 {
     protected $fillable = ['name', 'email', 'purpose', 'report_type', 'problem', 'image', 'job_id', 'company_id', 'company_name'];
-
-
+    
+    
     public function company()
-{
-    return $this->belongsTo(Company::class, 'company_id');
-}
-
-public function job()
-{
-    return $this->belongsTo(Job_task::class, 'job_id','id');
-}
-
+    {
+        return $this->belongsTo(Company::class, 'company_id');
+    }
+    
+    public function job()
+    {
+        return $this->belongsTo(Job_task::class, 'job_id','job_uuid');
+    }
+    
 }
